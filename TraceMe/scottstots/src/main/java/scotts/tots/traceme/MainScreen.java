6dd88c88fae04b71675d2a484736a52e1b72bfef
Aaron Villalpando <aaron2392@hotmail.com>
2014-03-09 18:32:16 -0500

    fixed array name

--- TraceMe/scottstots/src/main/java/scotts/tots/traceme/MainScreen.java
+++ TraceMe/scottstots/src/main/java/scotts/tots/traceme/MainScreen.java
@@ -10,19 +10,14 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
-
-import com.parse.ParseUser;
 
 import gamescreens.HomeScreenFrag;
 
@@ -47,7 +42,7 @@ public class MainScreen extends Activity {
         // mDrawer title is the title that appears when the drawer opens.
         mTitle = getResources().getString(R.string.title_activity_main_screen);
         mDrawerTitle = "Game Menu";
-        navMenuTitles = getResources().getStringArray(R.array.planets_array);
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_array);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
