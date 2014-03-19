619d281d02a3f562219f90a9d1c36378b7542f8a
Aaron Villalpando <aaron2392@hotmail.com>
2014-03-18 21:31:02 -0500

    renamed pointManager to customPath

---
+++ TraceMe/scottstots/src/main/java/helperClasses/CustomPath.java
@@ -0,0 +1,38 @@
+package helperClasses;
+
+/**
+ * Created by Aaron on 3/9/14.
+ */
+
+import java.util.ArrayList;
+
+/**
+ * This class manages one array of DataPoints
+ * One android path = 1 Point Manager = an array of datapoints
+ * **/
+public class CustomPath {
+    DataPoint point;
+    ArrayList<DataPoint> pointArray;
+    long startTime;
+    // We create a new CustomPath (an array) every time we create the first point in a path.
+    public CustomPath(float x, float y) {
+        pointArray = new ArrayList<DataPoint>();
+        startTime = System.currentTimeMillis();
+        pointArray.add(new DataPoint(x, y, 0));
+    }
+
+    public void addPoint(float x, float y) {
+        // We record the time difference between the last point and this one.
+        pointArray.add(new DataPoint(x, y, System.currentTimeMillis() - startTime));
+        startTime = System.currentTimeMillis();
+    }
+
+    public DataPoint get(int index) {
+        return pointArray.get(index);
+    }
+
+    public int size() {
+        return pointArray.size();
+    }
+
+}
