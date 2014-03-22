dd7f89ad10f21f01989d97a080af3d6348a28662
Niko Lazaris <nikolazaris@utexas.edu>
2014-03-21 20:07:45 -0500

    Login Screen +5 Sexiness

--- TraceMe/scottstots/src/main/java/scotts/tots/traceme/LoginNewUserActivity.java
+++ TraceMe/scottstots/src/main/java/scotts/tots/traceme/LoginNewUserActivity.java
@@ -36,13 +36,13 @@ public class LoginNewUserActivity extends Activity {
     private EditText usernameView;
     private EditText passwordView;
     private EditText passwordAgainView;
-    private ImageView validFormDrawable;
+//    private ImageView validFormDrawable;
 
     private boolean usernameValid;
     Button logInButton;
-    Button signUpButton;
-    Button facebookButton;
-    Button twitterButton;
+////    Button signUpButton;
+//    Button facebookButton;
+//    Button twitterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,19 +52,18 @@ public class LoginNewUserActivity extends Activity {
 
         usernameValid = false;
 
-        // Set up the signup form
-        signUpButton = (Button) findViewById(R.id.signUpButton);
-        signUpButton.setEnabled(false);
+//        // Set up the signup form
+//        signUpButton = (Button) findViewById(R.id.signUpButton);
+//        signUpButton.setEnabled(false);
 
         // Set up the login button
         logInButton = (Button) findViewById(R.id.logInButton);
-        logInButton.setEnabled(false);
-
+        
         // Set up the facebook / twitter button
-        facebookButton = (Button) findViewById(R.id.facebookButton);
-        twitterButton  = (Button) findViewById(R.id.twitterButton);
+//        facebookButton = (Button) findViewById(R.id.facebookButton);
+//        twitterButton  = (Button) findViewById(R.id.twitterButton);
 
-        validFormDrawable = (ImageView) findViewById(R.id.validFormDrawable);
+//        validFormDrawable = (ImageView) findViewById(R.id.validFormDrawable);
         usernameView = (EditText) findViewById(R.id.usernameField);
         passwordView = (EditText) findViewById(R.id.passwordField);
         // passwordAgainView = (EditText) findViewById(R.id.passwordAgain);
@@ -79,18 +78,18 @@ public class LoginNewUserActivity extends Activity {
                     @Override
                     public void done(ParseUser object, ParseException e) {
                         if (e != null && text.length() > 0) { // username available, so set green symbol
-                            validFormDrawable.setImageDrawable(getResources().getDrawable(
-                                    R.drawable.valid_form_green));
+//                            validFormDrawable.setImageDrawable(getResources().getDrawable(
+//                                    R.drawable.valid_form_green));
                             usernameValid = true;
-                            signUpButton.setEnabled(true);
-                            logInButton.setEnabled(false);
+//                            signUpButton.setEnabled(true);
+//                            logInButton.setEnabled(false);
 
                         } else {
                             usernameValid = false;
-                            signUpButton.setEnabled(false);
-                            logInButton.setEnabled(true);
-                            validFormDrawable.setImageDrawable(getResources().getDrawable(
-                                    R.drawable.valid_form_red));
+//                            signUpButton.setEnabled(false);
+//                            logInButton.setEnabled(true);
+//                            validFormDrawable.setImageDrawable(getResources().getDrawable(
+//                                    R.drawable.valid_form_red));
                         }
                     }
                 });
@@ -162,100 +161,100 @@ public class LoginNewUserActivity extends Activity {
 
 
         // Set up the submit button click handler
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Log.d("parseNetwork", "user: " + usernameView.toString());
-                // Validate the sign up data
-                boolean validationError = false;
+//        signUpButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                Log.d("parseNetwork", "user: " + usernameView.toString());
+//                // Validate the sign up data
+//                boolean validationError = false;
+//
+//                StringBuilder validationErrorMessage = new StringBuilder(getResources().getString(
+//                        R.string.error_intro));
+//                if (isEmpty(usernameView)) {
+//                    validationError = true;
+//                    validationErrorMessage.append(getResources().getString(
+//                            R.string.error_blank_username));
+//                }
+//
+//                // Error if username was not valid
+//                if (!usernameValid) {
+//                    validationError = true;
+//                    validationErrorMessage = new StringBuilder(getResources().getString(
+//                            R.string.error_username_taken));
+//                }
+//
+//                if (isEmpty(passwordView)) {
+//                    if (validationError) {
+//                        validationErrorMessage
+//                                .append(getResources().getString(R.string.error_join));
+//                    }
+//                    validationError = true;
+//                    validationErrorMessage.append(getResources().getString(
+//                            R.string.error_blank_password));
+//                }
+//             /*   if (!isMatching(passwordView, passwordAgainView)) {
+//                    if (validationError) {
+//                        validationErrorMessage
+//                                .append(getResources().getString(R.string.error_join));
+//                    }
+//                    validationError = true;
+//                    validationErrorMessage.append(getResources().getString(
+//                            R.string.error_mismatched_passwords));
+//                }*/
+//                validationErrorMessage.append(getResources().getString(R.string.error_end));
+//
+//                // If there is a validation error, display the error
+//                if (validationError) {
+//                    Log.d("parseNetwork",   "Validation Error!");
+//                    Toast.makeText(LoginNewUserActivity.this, validationErrorMessage.toString(),
+//                            Toast.LENGTH_LONG).show();
+//                    return;
+//                }
+//
+//                // Set up a progress dialog
+//                final ProgressDialog dlg = new ProgressDialog(LoginNewUserActivity.this);
+//                dlg.setTitle("Please wait.");
+//                dlg.setMessage("Signing up.  Please wait.");
+//                dlg.show();
+//
+//                // Set up a new Parse user
+//                ParseUser user = new ParseUser();
+//                user.setUsername(usernameView.getText().toString());
+//                user.setPassword(passwordView.getText().toString());
+//                // Call the Parse signup method
+//                user.signUpInBackground(new SignUpCallback() {
+//
+//                    @Override
+//                    public void done(ParseException e) {
+//                        dlg.dismiss();
+//                        if (e != null) {
+//                            // Show the error message
+//                            Toast.makeText(LoginNewUserActivity.this, e.getMessage(), Toast.LENGTH_LONG)
+//                                    .show();
+//                        } else {
+//                            // Start an intent for the dispatch activity
+//                            Intent intent = new Intent(LoginNewUserActivity.this, MainScreen.class);
+//                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                                    | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                            startActivity(intent);
+//                        }
+//                    }
+//                });
+//            }
+//        });
 
-                StringBuilder validationErrorMessage = new StringBuilder(getResources().getString(
-                        R.string.error_intro));
-                if (isEmpty(usernameView)) {
-                    validationError = true;
-                    validationErrorMessage.append(getResources().getString(
-                            R.string.error_blank_username));
-                }
-
-                // Error if username was not valid
-                if (!usernameValid) {
-                    validationError = true;
-                    validationErrorMessage = new StringBuilder(getResources().getString(
-                            R.string.error_username_taken));
-                }
-
-                if (isEmpty(passwordView)) {
-                    if (validationError) {
-                        validationErrorMessage
-                                .append(getResources().getString(R.string.error_join));
-                    }
-                    validationError = true;
-                    validationErrorMessage.append(getResources().getString(
-                            R.string.error_blank_password));
-                }
-             /*   if (!isMatching(passwordView, passwordAgainView)) {
-                    if (validationError) {
-                        validationErrorMessage
-                                .append(getResources().getString(R.string.error_join));
-                    }
-                    validationError = true;
-                    validationErrorMessage.append(getResources().getString(
-                            R.string.error_mismatched_passwords));
-                }*/
-                validationErrorMessage.append(getResources().getString(R.string.error_end));
-
-                // If there is a validation error, display the error
-                if (validationError) {
-                    Log.d("parseNetwork",   "Validation Error!");
-                    Toast.makeText(LoginNewUserActivity.this, validationErrorMessage.toString(),
-                            Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                // Set up a progress dialog
-                final ProgressDialog dlg = new ProgressDialog(LoginNewUserActivity.this);
-                dlg.setTitle("Please wait.");
-                dlg.setMessage("Signing up.  Please wait.");
-                dlg.show();
-
-                // Set up a new Parse user
-                ParseUser user = new ParseUser();
-                user.setUsername(usernameView.getText().toString());
-                user.setPassword(passwordView.getText().toString());
-                // Call the Parse signup method
-                user.signUpInBackground(new SignUpCallback() {
-
-                    @Override
-                    public void done(ParseException e) {
-                        dlg.dismiss();
-                        if (e != null) {
-                            // Show the error message
-                            Toast.makeText(LoginNewUserActivity.this, e.getMessage(), Toast.LENGTH_LONG)
-                                    .show();
-                        } else {
-                            // Start an intent for the dispatch activity
-                            Intent intent = new Intent(LoginNewUserActivity.this, MainScreen.class);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK
-                                    | Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-                        }
-                    }
-                });
-            }
-        });
-
-        // Set up the facebook button click event
-        facebookButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                loginWithFacebook();
-            }
-        });
-
-        // Set up the twitter button click event
-        twitterButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                loginWithTwitter();
-            }
-        });
+//        // Set up the facebook button click event
+//        facebookButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                loginWithFacebook();
+//            }
+//        });
+//
+//        // Set up the twitter button click event
+//        twitterButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View view) {
+//                loginWithTwitter();
+//            }
+//        });
     }
 
     public void loginWithFacebook() {
